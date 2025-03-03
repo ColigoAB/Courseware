267ee842b2b7d90c02bc5111835f38597eb5d5ee
Anteskog <72891738+Anteskog@users.noreply.github.com>
2025-03-03 21:51:12 +0100

    Script till labbar

---
+++ T2275 Advanced Intune Management of Windows 11/Detect.ps1
@@ -0,0 +1,29 @@
+﻿$apps = @(
+    "Microsoft.BingNews",
+    "Microsoft.BingWeather",
+    "Microsoft.GamingApp",
+    "Microsoft.GetHelp",
+    "Microsoft.Getstarted",
+    "Microsoft.MicrosoftSolitaireCollection",
+    "Microsoft.People",
+    "Microsoft.WindowsFeedbackHub",
+    "Microsoft.Microsoft.WindowsForOutlook",
+    "Microsoft.WindowsMaps",
+    "Microsoft.Xbox.TCUI",
+    "Microsoft.XboxGameOverlay",
+    "Microsoft.XboxGamingOverlay",
+    "Microsoft.XboxIdentityProvider",
+    "Microsoft.XboxSpeechToTextOverlay",
+    "Microsoft.YourPhone",
+    "Microsoft.ZuneMusic",
+    "Microsoft.ZuneVideo"
+)
+
+$foundApps = Get-AppxPackage | Where-Object { $_.Name -in $apps }
+if ($foundApps) {
+    Write-Output "Unwanted apps found"
+    Exit 1
+} else {
+    Write-Output "No unwanted apps found"
+    Exit 0
+}
---
+++ T2275 Advanced Intune Management of Windows 11/Remediate.ps1
@@ -0,0 +1,26 @@
+﻿$ErrorActionPreference = 'SilentlyContinue'
+$apps = @(
+    "Microsoft.BingNews",
+    "Microsoft.BingWeather",
+    "Microsoft.GamingApp",
+    "Microsoft.GetHelp",
+    "Microsoft.Getstarted",
+    "Microsoft.MicrosoftSolitaireCollection",
+    "Microsoft.MicrosoftOutlookForWindows",
+    "Microsoft.People",
+    "Microsoft.WindowsFeedbackHub",
+    "Microsoft.WindowsMaps",
+    "Microsoft.Xbox.TCUI",
+    "Microsoft.XboxGameOverlay",
+    "Microsoft.XboxGamingOverlay",
+    "Microsoft.XboxIdentityProvider",
+    "Microsoft.XboxSpeechToTextOverlay",
+    "Microsoft.YourPhone",
+    "Microsoft.ZuneMusic",
+    "Microsoft.ZuneVideo"
+)
+
+foreach ($app in $apps) {
+    Get-AppxPackage -Name $app | Remove-AppxPackage
+    Get-AppxProvisionedPackage -Online | Where-Object DisplayName -eq $app | Remove-AppxProvisionedPackage -Online
+}
