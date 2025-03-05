27b39efd8c07e7fe32ac108669da0ba10c74dfb4
Andreas Stenhall <andreas.stenhall@coligo.se>
2025-03-05 20:00:05 +0100

    Update Remediate.ps1

--- T2275 Advanced Intune Management of Windows 11/Remediate.ps1
+++ T2275 Advanced Intune Management of Windows 11/Remediate.ps1
@@ -1,4 +1,4 @@
-﻿$ErrorActionPreference = 'SilentlyContinue'
+$ErrorActionPreference = 'SilentlyContinue'
 $apps = @(
     "Microsoft.BingNews",
     "Microsoft.BingWeather",
@@ -20,7 +20,23 @@ $apps = @(
     "Microsoft.ZuneVideo"
 )
 
+# Process each app
 foreach ($app in $apps) {
-    Get-AppxPackage -Name $app | Remove-AppxPackage
+    # Remove for all users (this is the key change)
+    Get-AppxPackage -Name $app -AllUsers | Remove-AppxPackage -AllUsers
+    
+    # Remove provisioned packages
     Get-AppxProvisionedPackage -Online | Where-Object DisplayName -eq $app | Remove-AppxProvisionedPackage -Online
+}
+
+# Verify removal was successful
+$remainingApps = Get-AppxPackage -AllUsers | Where-Object { $_.Name -in $apps }
+$remainingProvisioned = Get-AppxProvisionedPackage -Online | Where-Object { $_.DisplayName -in $apps }
+
+if ($remainingApps -or $remainingProvisioned) {
+    # Some apps couldn't be removed
+    Exit 1
+} else {
+    # All apps were successfully removed
+    Exit 0
 }
